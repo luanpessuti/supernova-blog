951af5c8ddab34f8f2d2bf30e77d4261e7d70f1d
Luan Pessuti <luanpessuti@gmail.com>
2025-03-27 16:43:57 -0300

    update index.jsn

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config(); // Carrega variáveis do .env
-
 // Inicializa o Express
 const app = express();
 const port = process.env.PORT || 4000;
